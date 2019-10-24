32f5a0e2f758dc823fa7f29084a6f8a71bb82242
Nirav <niravjadatiya@gmail.com>
2019-10-24 16:56:06 +0530

    Routes fixed

--- Routes/web.php
+++ Routes/web.php
@@ -11,23 +11,19 @@
 |
 */
 
-Route::prefix('rightsmanagement')->group(function () {
-	Route::get('/', function () {
-		return view('rightsmanagement::index');
-	});
+Route::prefix('admins/rightsmanagement')->group(function () {
+    Route::get('/', 'RightsManagementController@index');
 
-	Route::group(['middleware' => ['role:super_admin']], function () {
-		// Role
-		Route::get('roles/datatable', 'RolesController@getDatatable');
-		Route::resource('roles', 'RolesController');
+    Route::group(['middleware' => ['auth:admin', 'role:super_admin']], function () {
+        // Role
+        Route::get('roles/datatable', 'RolesController@getDatatable');
+        Route::resource('roles', 'RolesController');
 
-		// Permission
-		Route::get('permission/datatable', 'PermissionController@getDatatable');
-		Route::resource('permission', 'PermissionController');
+        // Permission
+        Route::get('permission/datatable', 'PermissionController@getDatatable');
+        Route::resource('permission', 'PermissionController');
 
-		Route::get('role-permission/datatable', 'RolePermissionController@getDatatable');
-		Route::resource('role-permission', 'RolePermissionController');
-	});
-	// Route::get('admins/datatable', 'AdminController@getDatatable');
-	// Route::resource('admins', 'AdminController');
+        Route::get('role-permission/datatable', 'RolePermissionController@getDatatable');
+        Route::resource('role-permission', 'RolePermissionController');
+    });
 });
